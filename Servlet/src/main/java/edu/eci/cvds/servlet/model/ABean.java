3e93d7036bc730bed05a4344cd8d2b423ad4991c
SimonMarinM <german.marin@mail.escuelaing.edu.co>
2019-09-19 18:46:07 -0500

    Parte 4 terminada

--- Servlet/src/main/java/edu/eci/cvds/servlet/model/ABean.java
+++ Servlet/src/main/java/edu/eci/cvds/servlet/model/ABean.java
@@ -8,24 +8,31 @@ package edu.eci.cvds.servlet.model;
 import java.util.Random;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  *
  * @author 2146516
  */
 @ManagedBean(name="guessBean")
-@ApplicationScoped
+@SessionScoped
 public class ABean{
    private Random r = new Random();
    private int numeroAdivinar;
    private int premio;
    private int numeroIntentos=0;
-   private String ganador;
+   private String mensajeActual;
+   private List<Integer> intentos;
+   private boolean esGanador;
    public ABean(){
-       this.numeroAdivinar = r.nextInt(30);
+       this.numeroAdivinar = r.nextInt(30)+1;
        this.premio = 100000;
        this.numeroIntentos = 0;
-       this.ganador = "Aun no ha ganado el juego.";
+       this.mensajeActual = "Aun no ha ganado el juego.";
+       this.intentos = new ArrayList<Integer>();
+       this.esGanador = false;
    }
    public int getNumeroAdivinar(){
        return numeroAdivinar;
@@ -45,31 +52,59 @@ public class ABean{
    public void setNumeroIntentos(int numeroIntentos){
        this.numeroIntentos = numeroIntentos;
    }
-   public String getGanador(){
-       return ganador;
+   public String getMensajeActual(){
+       return mensajeActual;
    }
-   public void setGanador(String ganador){
-       this.ganador = ganador;
+   public void setMensajeActual(String mensajeActual){
+       this.mensajeActual = mensajeActual;
+   }
+   public List<Integer> getIntentos() {
+	   return intentos;
+   }
+   public void setIntentos(List<Integer> intentos) {
+	   this.intentos = intentos;
+   }
+   public void guess(String intento){
+	   try {
+		   int intentow = Integer.parseInt(intento);
+		   guess(intentow);
+	   }catch(java.lang.NumberFormatException e) {	
+		   if(!esGanador & premio > 0) {
+			   setMensajeActual("La entrada debe ser un entero...");
+		   }
+	   }
    }
    public void guess(int intento){
-       numeroIntentos +=1;
-       if (intento!=numeroAdivinar & premio>0){
-           ganador = "Incorrecto, Sigue intentando.";
-           premio -= 10000; 
-       }
-       else if (intento==numeroAdivinar & premio<=0){
-           setGanador("Adivinaste el numero pero quedaste sin premio.");
-       }
-       else if(intento==numeroAdivinar & premio>=0){
-           setGanador("Ganaste, tu premio es: "+premio+".");
-       }
+	       if(!esGanador & (intento > 30 || intento <= 0)  ) {
+	    	   setMensajeActual("El numero debe estar entre 1 y 30!...");		    	   
+	       }
+	       else if (!esGanador & intento!=numeroAdivinar & premio>0){
+	    	   numeroIntentos +=1;
+	    	   setMensajeActual("Incorrecto, Sigue intentando.");
+	           premio -= 10000;
+	           intentos.add(intento);
+	       }
+	       else if (premio<=0){	    	   
+	           setMensajeActual("Ya no puedes realizar mas intentos, por favor reinicie el juego.");	         
+	       }
+	       else if(!esGanador & intento==numeroAdivinar & premio>=0){
+	    	   numeroIntentos +=1;
+	           setMensajeActual("Ganaste, tu premio es: "+premio+".");
+	           this.esGanador = true;
+	       }
+	       else {
+	    	   setMensajeActual("Premio: "+premio+". Pulse reiniciar para jugar con nosotros de nuevo.");
+	       }
+
        
    }
    public void reStart(){
-       numeroAdivinar = r.nextInt(30);
-       premio = 100000;
-       this.ganador = "Aun no ha ganado el juego.";
+       this.numeroAdivinar = r.nextInt(30);
+       this.premio = 100000;
+       this.mensajeActual = "Aun no ha ganado el juego.";
        this.numeroIntentos = 0;
+       this.intentos.clear();
+       this.esGanador = false;
    }
    
    
